0eecd7c82fda4ac16d537c4769aa207aa57e1f63
Martin Řehánek <martin.rehanek@gmail.com>
2022-12-14 12:32:17 +0100

    Enable downloading extraction log

--- src/app/validations/validation/validation.component.ts
+++ src/app/validations/validation/validation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AppSettingsService } from 'src/app/services/app-settings.service';
 import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
@@ -12,7 +13,7 @@ export class ValidationComponent implements OnInit {
   id = "";
   validation = undefined;
 
-  constructor(private backend: BackendService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private backend: BackendService, private router: Router, private route: ActivatedRoute, private appSettings: AppSettingsService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
@@ -32,6 +33,26 @@ export class ValidationComponent implements OnInit {
     this.backend.cancelValidation(this.id).subscribe(result => {
       this.loadValidation(this.id);
     })
+  }
+
+  extractionLogAvailable() {
+    switch (this.validation!['state']) {
+      case 'READY_FOR_EXECUTION':
+      case 'TO_BE_EXECUTED':
+      case 'EXECUTING':
+      case 'FINISHED':
+      case 'ERROR': //nekdy muze byt prazdny, pokud error nastal treba pri rozbalovani
+      case 'TO_BE_ARCHIVED':
+      case 'ARCHIVING':
+      case 'ARCHIVED':
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  extractionLogUrl() {
+    return `${this.appSettings.adminApiBaseUrl}/results/${this.validation!['id']}/extraction-log`
   }
 
 }
